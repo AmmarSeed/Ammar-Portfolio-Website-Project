b447f808209875dd921775fe58eb3c2c76dbd528
Ammar <102736025+AmmarSeed@users.noreply.github.com>
2022-04-29 01:16:32 +0200

    website project

---
+++ script.js
@@ -0,0 +1,25 @@
+// navigation bar 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%" 
+}
+
+
+// About section pics slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
