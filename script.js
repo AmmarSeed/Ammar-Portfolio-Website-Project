b159f7a58df2708ea1df9bc8b3aeedc79637a70e
Ammar <102736025+AmmarSeed@users.noreply.github.com>
2022-04-29 17:13:28 +0200

    potfolio website js changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function openNav() {
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%" 
+  document.getElementById("myNav").style.width = "0%";
 }
 
 
@@ -19,7 +19,7 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex > slides.length) {slideIndex = 1;}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
